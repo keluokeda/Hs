61b446a9165860760e88d65f61cacc22d13cedf4
ake <913918146@qq.com>
2024-05-21 08:30:31 +0800

    1，日常提交。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ke.hs"
         minSdk = 26
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.1.6"
+        versionCode = 17
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
